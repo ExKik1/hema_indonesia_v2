b4ab0db8c1973029c9dce5635a627d16637cfc95
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2025-03-24 20:40:26 +0700

    Initial Commit

---
+++ resources/views/template/layout-main.blade.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Hema Indonesia</title>
+    {{-- main css --}}
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet" />
+    {{-- font urbanist --}}
+    <link rel="stylesheet" href="{{ asset('library/font/urbanist.css') }}">
+    <!-- swiper css -->
+    <link rel="stylesheet" href="{{ asset('library/swiper/swiper-bundle.min.css') }}" />
+    <!-- link font icon -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"
+        integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    @vite('resources/css/app.css')
+</head>
+
+<body class="font-urbanist">
+    <!-- header and navbar menu -->
+    <header id="navbar" class="w-full left-0 top-0 z-50 shadow-sm border-b-[0.5px] border-gray-200">
+        <nav class="container flex items-center justify-between h-16 sm:h-20">
+            <a href="" class="text-xl sm:text-2xl font-bold">
+                <span class="text-primary">Hema</span>.Indonesia
+            </a>
+            <div id="nav-menu"
+                class="absolute top-0 left-[-100%] w-full min-h-[100vh] bg-white lg:backdrop-blur-none flex items-start justify-start p-6 lg:p-0 duration-300 z-50 overflow-hidden lg:static lg:min-h-fit lg:bg-transparent lg:w-auto">
+                <ul class="flex flex-col w-full lg:items-center lg:gap-8 lg:flex-row">
+                    <h5 class="font-bold lg:hidden text-lg inline-block absolute top-6 left-6">
+                        <span class="text-primary">Hema</span>.Indonesia
+                    </h5>
+                    <li class="mt-20 lg:mt-0 border-b-[0.5px] border-gray-300 lg:border-none w-full py-4"><a
+                            href="" class="nav-link">Home</a></li>
+                    <li class="border-b-[0.5px] border-gray-300 lg:border-none w-full py-4 outline-none">
+                        <a href="" class="nav-link">Tentang</a>
+                    </li>
+                    <li class="border-b-[0.5px] border-gray-300 lg:border-none w-full py-4">
+                        <a href="" class="nav-link">Produk</a>
+                    </li>
+                    <li class="border-b-[0.5px] border-gray-300 lg:border-none w-full py-4"><a href=""
+                            class="nav-link">Galeri</a></li>
+                    <li class="border-b-[0.5px] border-gray-300 lg:border-none w-full py-4"><a href=""
+                            class="nav-link">Order</a></li>
+                </ul>
+            </div>
+            <div class="text-lg sm:2xl cursor-pointer z-50 lg:hidden">
+                <i class="fa-solid fa-bars-staggered" id="hamburger"></i>
+            </div>
+        </nav>
+    </header>
+
+    <div class="z-10 fixed right-[5%] lg:right-[3%] xl:right-[2%] bottom-[5%] flex flex-col gap-2">
+        <!-- scroll up -->
+        <a href="#hero"
+            class="scroll_up_box text-gray-700 hover:bg-black hover:text-white rounded bg-white w-10 h-10 hidden duration-300 justify-center items-center border border-gray-200">
+            <i class="fas fa-chevron-up"></i>
+        </a>
+        <!-- whatsapp -->
+        <a href="https://wa.me/6281389861954?text=Hallo"
+            class="whatsapp-box text-xl text-gray-700 hover:bg-black hover:text-white rounded bg-white w-10 h-10 duration-300 flex justify-center items-center border border-gray-200">
+            <i class="fab fa-whatsapp"></i>
+        </a>
+    </div>
+
+    @yield('content-main')
+
+    <footer class="pt-10 pb-8 shadow border-t-[0.5px] border-gray-200">
+        <main class="container">
+            <div class="grid grid-cols-1 lg:grid-cols-3 gap-10 lg:gap-24 md:gap-14 mb-4">
+                <div class="block">
+                    <h4 class="font-semibold mb-2">
+                        <span class="text-primary">Hema</span>.Indonesia
+                    </h4>
+                    <p class="text-sm text-justify">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi
+                        aperiam fugit eos repellat distinctio consequatur ad cumque, nisi
+                        architecto laudantium numquam nesciunt. Soluta aspernatur iste sed
+                        maiores optio dicta a!
+                    </p>
+                </div>
+                <div class="block">
+                    <h4 class="font-medium mb-2">Permalinks</h4>
+                    <div class="flex gap-2 flex-col text-sm">
+                        <a href="" class="nav-link hover:translate-x-1 duration-300">Home</a>
+                        <a href="" class="nav-link hover:translate-x-1 duration-300">Tentang</a>
+                        <a href="" class="nav-link hover:translate-x-1 duration-300">Produk</a>
+                        <a href="" class="nav-link hover:translate-x-1 duration-300">Galeri</a>
+                        <a href="" class="nav-link hover:translate-x-1 duration-300">Order</a>
+                    </div>
+                </div>
+                <div class="block">
+                    <h4 class="font-medium mb-2">News Letter</h4>
+                    <p class="text-sm">Subscribe to receive updates, access to exclusive deals, and more.</p>
+                    <div class="block text-sm mt-3">
+                        <input type="text"
+                            class="w-full border-[1.5px] rounded-sm border-gray-600 focus:border-primary px-4 py-[10px] outline-none"
+                            name="" id="" placeholder="E-mail">
+                        <button
+                            class="bg-gradient-to-r from-primary to-secondary hover:opacity-90 rounded-sm font-medium tracking-widest inline-block mt-3 px-4 py-[10px] outline-none text-white"
+                            type="button">Subscribe</button>
+                    </div>
+                </div>
+            </div>
+            </div>
+
+            <div class="social-media mt-10 md:mt-6 flex gap-3">
+                <a href="" title="Instagram"
+                    class="w-9 h-9 flex justify-center items-center hover:bg-primary shadow hover:shadow-none hover:text-white rounded">
+                    <i class="fab fa-instagram"></i>
+                </a>
+                <a href="" title="Tiktok"
+                    class="w-9 h-9 flex justify-center items-center hover:bg-primary shadow hover:shadow-none hover:text-white rounded">
+                    <i class="fab fa-tiktok"></i>
+                </a>
+                <a href="" title="X"
+                    class="w-9 h-9 flex justify-center items-center hover:bg-primary shadow hover:shadow-none hover:text-white rounded">
+                    <i class="fab fa-x-twitter"></i>
+                </a>
+                <a href="" title="Youtube"
+                    class="w-9 h-9 flex justify-center items-center hover:bg-primary shadow hover:shadow-none hover:text-white rounded">
+                    <i class="fab fa-youtube"></i>
+                </a>
+            </div>
+            <p class="text-sm mt-8">
+                &copy; 2025 - <span class="text-primary">Hema</span>.Indonesia | All Rights Reserved
+            </p>
+        </main>
+    </footer>
+
+    <script src="{{ asset('library/swiper/swiper-bundle.min.js') }}"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
+</body>
+
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#b17457",
+                secondary: "#c29470",
+            },
+        },
+        screens: {
+            sm: "340px",
+            md: "540px",
+            lg: "768px",
+            xl: "1180px",
+        },
+        fontFamily: {
+            urbanist: ["Urbanist", "sans-serif"],
+        },
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: "12px",
+                md: "32px",
+            },
+        },
+    },
+    plugins: [],
+};
